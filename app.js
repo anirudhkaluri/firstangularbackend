5d711b97fdd5b11cc603ad47fac6436263469a49
anirudhkaluri <anirudhkaluri@gmail.com>
2023-04-07 21:56:58 +0530

    exported the user model and connected to the mongodb

--- app.js
+++ app.js
@@ -1,16 +1,36 @@
 const express=require("express");
 const app=new express();
+const dotenv=require('dotenv');
+dotenv.config();
+const mongoose=require('mongoose');
+
+
 
 const profileRouter=require('./Routes/profileRoutes');
 
 app.use(express.json());
+app.use(express.urlencoded());
 
 app.use('/profile',profileRouter);
 
 
 const PORT=process.env.PORT||8000;
 
+const dbURI=process.env.db_connection;
 
-app.listen(PORT,()=>{
-    console.log(`listening to PORT ${PORT}`);
-});
+async function connectDB(){
+    try{
+        await mongoose.connect(dbURI);
+        app.listen(PORT,()=>{
+            console.log(`listening to PORT ${PORT}`);
+        });
+    }
+    catch(error){
+        console.log(`error connecting to MongoDB ${error}`);
+    }
+}
+
+connectDB();
+
+
+

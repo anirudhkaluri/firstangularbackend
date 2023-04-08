8bd27da0a7423fee7ef1cbef392bffd699bbfb27
anirudhkaluri <anirudhkaluri@gmail.com>
2023-04-08 15:56:39 +0530

    developed login and register services

---
+++ Services/authServices.js
@@ -0,0 +1,40 @@
+const jwt=require('jsonwebtoken');
+const dotenv=require('dotenv');
+dotenv.config();
+const TOKEN_KEY=process.env.TOKEN_KEY.toString();
+
+
+const generate_token=(uid)=>{
+
+    const token=jwt.sign({user:uid},TOKEN_KEY,{
+        expiresIn:"2h"
+    });
+    return token;
+
+}
+
+
+const verify_token=(req,res,next)=>{
+
+    const auth_header=req.headers['authorization'];
+    const token=auth_header && auth_header.split(' ')[1];
+
+    if(token===null)
+        return res.status(401).json({isLogin:false});
+
+    jwt.verify(token,process.env.TOKEN_key.toString(),(err,user)=>{
+        if(err)
+            return res.status(403).json({isLogin:false});
+        else{
+            req.user=user;
+            res.locals.isLogin=true;
+        }
+    });
+    next();
+    
+}
+
+module.exports={
+    verify_token,
+    generate_token
+}
--- Services/profileServices.js
+++ Services/profileServices.js
@@ -1,13 +1,40 @@
+const {generate_token}=require('./authServices');
+const {User}=require('../Models/user');
+const {getUser}=require('../Dao/profileDao');
 
+const login_user=async (req,res)=>{
+    
+    const userDetails=req.body;
+    let user=await getUser(userDetails.user_name);
+    if(user){
+        if(comparePassword(user,userDetails)){
+            const token=generate_token(user._id);
+            return res.json({userExists:true,jwt:token});
+        }
+        else
+            return res.json({userExists:false});
+        
+    }
+    res.json({userExists:false});
 
-
-const login_user=(req,res)=>{
-    res.send("hi");
 }
 
-const register_user=(req,res)=>{
-    res.send("hi");
+const register_user=async (req,res)=>{
+    
+    const userDetails=req.body;
+    //check if user already present
+    let user=await getUser(userDetails.user_name);
+
+    //if user already present
+    if(user)
+        return res.json({userExists:true});
+    
+    //if user not present
+    user=await createUser(userDetails);
+    const token=generate_token(user._id);
+    return res.json({userExists:false,jwt:token});
 }
+
 
 module.exports={
     login_user,

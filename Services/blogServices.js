a2bcd56a1aa9dc1735822c21caa46f2b2ad22ad8
anirudhkaluri <anirudhkaluri@gmail.com>
2023-04-09 12:45:05 +0530

    created blog services template

---
+++ Services/blogServices.js
@@ -0,0 +1,25 @@
+
+
+
+const get_all_blogs=(req,res)=>{
+
+}
+
+const get_my_blogs=(req,res)=>{
+
+}
+
+const delete_blog=(req,res)=>{
+
+}
+
+const modify_blog=(req,res)=>{
+
+}
+
+module.exports={
+    get_all_blogs,
+    get_my_blogs,
+    delete_blog,
+    modify_blog
+}

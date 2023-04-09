aea79ede8076ba650f841df1ffc69c64caaa34c5
anirudhkaluri <anirudhkaluri@gmail.com>
2023-04-09 15:01:34 +0530

    blog model modified

--- Models/blog.js
+++ Models/blog.js
@@ -1,7 +1,7 @@
 const mongoose= require('mongoose');
 const Schema=mongoose.Schema;
 const blogSchema=new Schema({
-    user_id:{
+    user:{
         type:mongoose.Schema.Types.ObjectId,
         ref:'User'
     },
